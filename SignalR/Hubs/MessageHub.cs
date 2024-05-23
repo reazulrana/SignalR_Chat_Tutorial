e76b3eb6b9998c355c3e6167c68cc4c15bdb7aae
Reazul Islam <rana12312344@gmail.com>
2024-05-23 17:42:55 +0600

    working on specific user

--- SignalR/Hubs/MessageHub.cs
+++ SignalR/Hubs/MessageHub.cs
@@ -12,18 +12,40 @@ namespace SignalR.Hubs
 
         public Task SendMessageToAll(string message)
         {
-            return Clients.All.SendAsync("ReceiveMessage", message);
+            return Clients.All.SendAsync("AgentGroup", message);
         }
-
-
-        public Task sendtocaller(string message)
+        public Task SendMessageToUser(string connectionid,string message)
+        {
+            return Clients.Client(connectionid).SendAsync("AgentGroup", message);
+        }
+        public Task SendToCaller(string message)
         {
             return Clients.Caller.SendAsync("AgentGroup", message);
         }
+
         public Task SendMessageToAgent(string message)
         {
             return Clients.All.SendAsync("AgentGroup", message);
         }
+        public Task JoinGroup(string group)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+        public Task SendMessageToGroup(string group, string message)
+        {
+            return Clients.Group(group).SendAsync("AgentGroup", message);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+            await base.OnDisconnectedAsync(exception);
+        }
+
 
 
     }
